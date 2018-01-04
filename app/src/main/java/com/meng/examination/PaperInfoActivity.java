ccc3d50ab1cbbeaa6e4ea8ac3100b76093830d41
imagedt_xindemeng <dingbang@imagedt.com>
2018-01-04 13:52:35 +0800

    *

--- app/src/main/java/com/meng/examination/PaperInfoActivity.java
+++ app/src/main/java/com/meng/examination/PaperInfoActivity.java
@@ -1,26 +1,25 @@
 package com.meng.examination;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
-public class PaperListActivity extends AppCompatActivity {
+public class PaperInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_paper_list);
+        setContentView(R.layout.activity_paper_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
-    public void paperInfo(View view) {
-        Intent intent = new Intent(PaperListActivity.this, PaperInfoActivity.class);
-        startActivity(intent);
+        TextView textView = (TextView) findViewById(R.id.paperTitle);
+        textView.setBackgroundColor(Color.GRAY);
     }
 
 }
