4a750fd24d213309f3c76b79f875b04b60e977a7
shameng <879163994@qq.com>
2018-01-07 22:29:48 +0800

    *

--- app/src/main/java/com/meng/examination/PaperResultActivity.java
+++ app/src/main/java/com/meng/examination/PaperResultActivity.java
@@ -1,28 +1,23 @@
 package com.meng.examination;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.TextView;
 
-public class PaperInfoActivity extends AppCompatActivity {
+public class PaperResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_paper_info);
+        setContentView(R.layout.activity_paper_result);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        TextView textView = (TextView) findViewById(R.id.paperTitle);
-        textView.setBackgroundColor(Color.GRAY);
     }
 
-    public void doPaper(View view) {
-        Intent intent = new Intent(PaperInfoActivity.this, PaperDetailActivity.class);
+    public void analysis(View view) {
+        Intent intent = new Intent(PaperResultActivity.this, PaperAnalysisActivity.class);
         startActivity(intent);
     }
 
